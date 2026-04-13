77a2015879afceaa8119feb21238ebeb35d966d1
GabrielSkold <gabriel.skold@hotmail.se>
2026-04-13 15:47:06 +0200

    fixed issues when calling on img from api

--- src/scripts/products.js
+++ src/scripts/products.js
@@ -1,26 +1,28 @@
 import { getProducts } from "../utils/productsApi.js";
-import { updateCartCount } from "./cart.js"
+import { updateCartCount } from "./cart.js";
 updateCartCount();
 const container = document.querySelector("#products");
 
 const init = async () => {
-    if(!container) return;
+  if (!container) return;
 
-    container.innerHTML = "<p>Loading...</p>";
-    try {
-        const products = await getProducts();
-        if(products.length === 0) {
-            container.innerHTML = "No products found.";
-            return
-        }
-        console.log(products);
-    
-        container.innerHTML = products.map(product => `
+  container.innerHTML = "<p>Loading...</p>";
+  try {
+    const products = await getProducts();
+    if (products.length === 0) {
+      container.innerHTML = "No products found.";
+      return;
+    }
+    console.log(products);
+
+    container.innerHTML = products
+      .map(
+        (product) => `
             <a class="product-href" href="product.html?id=${product._id}">
                 <article class="product-card">
                     <img
                         class="product-card__image"
-                        src="${product.images[0].url}"
+                        src="${product.images}"
                         alt="${product.name}"
                     />
                     <div class="product-card__body">
@@ -31,14 +33,16 @@ const init = async () => {
                     </div>
                 </article>
             </a>
-        `).join("");
-    } catch (error) {
-        console.error("Failed to load products:", error);
-        console.log(error);
+        `,
+      )
+      .join("");
+  } catch (error) {
+    console.error("Failed to load products:", error);
+    console.log(error);
 
-        container.innerHTML = `
+    container.innerHTML = `
         <p style="color: red;">Failed to load products. Please try again.</p>
         `;
-    }
+  }
 };
 init();
