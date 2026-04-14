b959a2e34686ebcc44447cdc22de2c6bc45c4bec
GabrielSkold <gabriel.skold@hotmail.se>
2026-04-14 13:31:11 +0200

    added checkout logic and rendering out items from cart

--- src/scripts/checkout.js
+++ src/scripts/checkout.js
@@ -1,4 +1,53 @@
 import { getCart, updateCartCount } from "../utils/cartUtils.js";
 
-const cart = getCart();
+const checkoutContainer = document.querySelector("#checkout-items");
+const checkoutTotal = document.querySelector("#checkout-total");
+const placeOrderButton = document.querySelector("#place-order-button");
+
+const renderCheckout = () => {
+  if (!checkoutContainer) {
+    return;
+  }
+  const cart = getCart();
+
+  if (cart.length === 0) {
+    checkoutContainer.innerHTML = `<p>No items in cart</p>`;
+
+    if (checkoutTotal) {
+      checkoutTotal.textContent = "";
+    }
+
+    if (placeOrderButton) {
+      placeOrderButton.disabled = true;
+    }
+    return;
+  }
+
+  checkoutContainer.innerHTML = cart
+    .map(
+      (item) => `
+    <div class="checkout-item">
+      <img src="${item.image}" alt="${item.name}" />
+      <h3>${item.name}</h3>
+      <p>Size: ${item.size}</p>
+      <p>Quantity: ${item.quantity}</p>
+      <p>Price: ${item.price}kr</p>
+    </div>
+  `,
+    )
+    .join("");
+
+  const total = cart.reduce((sum, item) => {
+    return sum + item.price * item.quantity;
+  }, 0);
+
+  if (checkoutTotal) {
+    checkoutTotal.textContent = `Total: ${total}kr`;
+  }
+
+  if (placeOrderButton) {
+    placeOrderButton.disabled = false;
+  }
+};
 updateCartCount();
+renderCheckout();
