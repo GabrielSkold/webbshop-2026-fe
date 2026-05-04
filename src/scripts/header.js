38f4c629ccada8bfebf5900b9765e552afdea7a3
bouchardhenry <bouchardhenry@gmail.com>
2026-05-04 09:48:15 +0200

    fix: prevent infinite loop in brand-links ticker on mobile

--- src/scripts/header.js
+++ src/scripts/header.js
@@ -43,7 +43,7 @@ document.addEventListener("click", (e) => {
 });
 
 const ul = document.querySelector('.brand-links')
-while (ul.scrollWidth < window.innerWidth * 2) {
+for (let i = 0; i < 6 && ul.scrollWidth < window.innerWidth * 2; i++) {
     ul.innerHTML += ul.innerHTML
 }
 
